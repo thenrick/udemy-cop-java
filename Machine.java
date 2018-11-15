fb3fb18b0e0ffc8ac4e4332581490e91daad5e0e
Tom Henricksen <tom.henricksen@gmail.com>
2018-11-14 20:46:59 -0500

    Udemy Java Course from CaveofProgramming.com
    Udemy Java Course from CaveofProgramming.com - Tutorial 38

---
+++ Machine.java
@@ -0,0 +1,22 @@
+
+public abstract class Machine {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public abstract void start();
+    public abstract void doStuff();
+    public abstract void shutdown();
+    
+    public void run() {
+        start();
+        doStuff();
+        shutdown();
+    }
+}
